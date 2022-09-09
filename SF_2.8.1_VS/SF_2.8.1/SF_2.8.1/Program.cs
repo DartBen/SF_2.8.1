1865d1585d5bed42c545686a4939588abfc617c5
Dmitry <ryabchikk@rambler.ru>
2022-09-09 11:42:20 +0300

    VS_Zero

---
+++ SF_2.8.1_VS/SF_2.8.1/SF_2.8.1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SF_2._8._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
